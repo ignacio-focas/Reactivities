21dad9b3d5f0d3e225606fb33a3252295420e2f7
ignacio-focas <ignacio.focas@moxit.com.ar>
2022-11-23 15:35:37 -0300

    Add CorsPolicy para fetching desde distinto origen

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const apiUrl = "http://localhost:7092/api/activities";
+
+  const [activities, setActivities] = useState([]);
+
+  useEffect(()=>{
+    axios.get(apiUrl).then(response=>{
+      setActivities(response.data);
+    })
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((a: any) => (
+            <li key={a.id}>
+              {a.title}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
