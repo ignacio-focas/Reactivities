86d7eadc248f268f300ec76d65abaed66802adb6
ignacio-focas <ignacio.focas@moxit.com.ar>
2022-11-24 14:20:33 -0300

    reestructurado de carpetas y tipo activity en ts

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import { List, Header } from 'semantic-ui-react';
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import { Activity } from './../models/activity';
 
 function App() {
 
   const apiUrl = "http://localhost:7092/api/activities";
 
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Array<Activity>>([]);
 
   useEffect(()=>{
     axios.get(apiUrl).then(response=>{
@@ -20,7 +19,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Reactivities'/>
         <List>
-          {activities.map((a: any) => (
+          {activities.map((a) => (
             <List.Item key={a.id}>
               {a.title}
             </List.Item>
