e1f5004dd0caf9c419beb0534287133057af20bd
ignacio-focas <ignacio.focas@moxit.com.ar>
2022-11-24 15:49:21 -0300

    manejo del display del formulario edit

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -12,6 +12,7 @@ function App() {
 
   const [activities, setActivities] = useState<Array<Activity>>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(()=>{
     axios.get<Array<Activity>>(apiUrl).then(response=>{
@@ -27,15 +28,27 @@ function App() {
     setSelectedActivity(undefined);
   }
 
+  function handleFormOpen(id?:string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
   return (
     <>
-      <NavBar/>
+      <NavBar openForm={handleFormOpen}/>
       <Container style={{marginTop: '7em'}} >
         <ActivityDashboard 
           activities={activities} 
           selectedActivity={selectedActivity}
           selectActivity = {handleSelectActivity}
-          cancelSelectActivity = {handleCancelSelectActivity} />
+          cancelSelectActivity = {handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose} />
       </Container>
     </>
   );
