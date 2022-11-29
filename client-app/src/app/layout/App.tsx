525328788d1a9776b46118b97577b7b2c9e8aca6
ignacio-focas <ignacio.focas@moxit.com.ar>
2022-11-29 14:57:46 -0300

    mostrar errores de validacion y más manejo d errores

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar';
 import ActivityDashboard from './../../features/activities/dashboard/ActivityDashboard';
-import { observer } from 'mobx-react-lite';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from './../../features/activities/details/ActivityDetails';
 import TestErrors from './../../features/errors/TestError';
-import { ToastContainer } from 'react-toastify';
 import NotFound from './../../features/errors/NotFound';
 import { Container } from 'semantic-ui-react';
+import { ToastContainer } from 'react-toastify';
+import { observer } from 'mobx-react-lite';
 
 
 function App() {
